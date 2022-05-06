be30baeab6cb372613bb9c8e64da7ad8391a7bf8
Adam Krček <adam.krcek@avast.com>
2022-05-06 16:54:05 +0200

    removed atomic

--- main.cpp
+++ main.cpp
@@ -10,31 +10,33 @@ using std::endl;
 using namespace std::chrono;
 
 #define ARRCOUNT 5
+#define ARRSIZE 64
 
 int main()
 {
     srand(time(0));
-    int arr[ARRCOUNT][8];
+    int arr[ARRCOUNT][ARRSIZE];
     for (int i = 0; i < ARRCOUNT; i++)
     {
-        for (int j = 0; j < 8; j++)
+        for (int j = 0; j < ARRSIZE; j++)
         {
-            arr[i][j] = (rand() % 10);
+            arr[i][j] = (rand() % 100);
         }
     }
 
-    SortingNetwork *sorter = new SortingNetwork(8);
+    SortingNetwork *sorter = new SortingNetwork(ARRSIZE);
 
     for (int i = 0; i < ARRCOUNT; i++)
     {
         auto start = high_resolution_clock::now();
         sorter->sort(arr[i]);
-        //sort_odd_even8(arr[i], true); // norm: 1400, par: 2349900
+        // sort_odd_even8(arr[i], true); // norm: 1400, par: 2349900
         auto stop = high_resolution_clock::now(); // time: 4300
 
         auto duration = duration_cast<nanoseconds>(stop - start);
 
-        for(int j=0; j < 8; j++){
+        for (int j = 0; j < ARRSIZE; j++)
+        {
             cout << arr[i][j] << ',';
         }
         cout << endl;
